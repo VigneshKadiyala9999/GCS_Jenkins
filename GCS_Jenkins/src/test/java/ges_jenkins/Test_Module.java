2f4706109191eb21677f6c4f537e4fa9455d4e1d
VigneshKadiyala9999 <kssvignesh1999@gmail.com>
2023-10-09 18:19:23 +0530

    Test

--- GCS_Jenkins/src/test/java/ges_jenkins/Test_Module.java
+++ GCS_Jenkins/src/test/java/ges_jenkins/Test_Module.java
@@ -2,7 +2,10 @@ package ges_jenkins;
 
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -33,7 +36,7 @@ public class  Test_Module
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//button[@class='ant-btn']")).click();
 		Thread.sleep(3000);
-		System.out.println("Home Page");
+		System.out.println("Home Page"); 
 	}
 	@Test
 	public void LOGEFFOTS() throws Exception
@@ -116,5 +119,47 @@ public class  Test_Module
 		Thread.sleep(2000);
 		//driver.findElement(By.xpath("//span[text()='OK']")).click();
 	}
-
+	
 }
+	/* @Test
+	 public void TravelDesk() throws Exception
+	 {
+	    WebDriver driver = new ChromeDriver();
+		driver.get("http://20.219.145.143/signin");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("GI2600",Keys.TAB,"@123#",Keys.ENTER);
+		driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+		Thread.sleep(3000);
+	      	/*Travel Request
+		System.out.println("Travel Desk");
+	    driver.findElement(By.xpath("(//div[@Class='ant-menu-submenu-title'])[6]")).click();
+	    driver.findElement(By.xpath("//a[@href='/travel_desk/request']")).click();
+	    driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-link']")).click();
+		driver.findElement(By.xpath("//input[@class='ant-select-selection-search-input']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[contains(text(),'GCS HR Initiative')]")).click();
+	    driver.findElement(By.xpath("(//div[@class='ant-form-item-control-input-content'])[2]"));
+		driver.findElement(By.xpath("(//input[@class='ant-input'])[1]"));
+		driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys("Hyderabad");
+		driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("USA");
+		driver.findElement(By.xpath("(//div[@class='ant-picker'])[1]"));
+		driver.findElement(By.xpath("(//div[@class='ant-picker-input'])[1]"));
+		driver.findElement(By.xpath("(//input[@placeholder='Select date'])[1]"));
+		driver.findElement(By.xpath("(//span[@class='anticon anticon-calendar'])[1]"));
+		driver.findElement(By.xpath("//input[@id='newTravelRequest_journeyDate']")).click();
+		driver.findElement(By.xpath("//div[normalize-space()='31']")).click();
+		driver.findElement(By.xpath(" //input[@id='newTravelRequest_returnJourneyDate']")).click();
+		Thread.sleep(3000);
+	  WebElement E1 =  driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div[1]/div[1]/button[3]"));
+	   E1.click();
+	   
+		//driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div[1]/div[2]/table/tbody/tr[5]/td[4]")).click();
+		//driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div[1]/div[2]/table/tbody/tr[5]/td[5]")).click();
+		//driver.findElement(By.xpath("//td[@title='2023-08-31']//div[@class='ant-picker-cell-inner'][normalize-space()='31']")).click();
+		//driver.findElement(By.xpath("(//input[@type='text'])[3]")).sendKeys("9893416256");
+		//driver.findElement(By.xpath("//textarea[@class='ant-input']")).sendKeys("On Site Project");
+		//driver.findElement(By.xpath("//button[@type='submit']")).click();
+	 */
+  
+
